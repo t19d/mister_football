d7720b123d2d7693e4642003ed8ceded37c8aea6
David <davidtojo99@gmail.com>
2020-05-24 20:13:29 +0200

    Ordenar por hora y fecha

--- lib/routes/partidos/w_lista_partidos.dart
+++ lib/routes/partidos/w_lista_partidos.dart
@@ -24,11 +24,24 @@ class _ListaPartidos extends State<ListaPartidos> {
   Widget itemPartidos() {
     final boxPartidos = Hive.box('partidos');
     final boxPerfil = Hive.box('perfil');
+    List partidosOrdenados = [];
+    for (var i = 0; i < boxPartidos.length; i++) {
+      partidosOrdenados.add([i, boxPartidos.getAt(i).fecha, boxPartidos.getAt(i).hora]);
+    }
+    //Ordenar por hora
+    partidosOrdenados.sort((a, b) => (a[2]).compareTo(b[2]));
+    //Ordenar por fecha
+    partidosOrdenados.sort((a, b) => (a[1]).compareTo(b[1]));
+    print(partidosOrdenados);
+    Map<String, dynamic> perfil = {"nombre_equipo": "Mi equipo", "escudo": "", "modo_oscuro": false};
+    if (boxPerfil.get(0) != null) {
+      perfil = Map.from(boxPerfil.get(0));
+    }
     if (boxPartidos.length > 0) {
       return ListView(
         shrinkWrap: true,
         children: List.generate(boxPartidos.length, (iPartido) {
-          final Partido partidoBox = boxPartidos.getAt(iPartido) as Partido;
+          final Partido partidoBox = boxPartidos.getAt(partidosOrdenados[iPartido][0]) as Partido;
           return Card(
             shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.circular(15.0),
@@ -46,40 +59,83 @@ class _ListaPartidos extends State<ListaPartidos> {
                 );
               },
               child: Container(
-                decoration: BoxDecoration(borderRadius: BorderRadius.circular(15)),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: <Widget>[
-                    //Representar el tipo de partido cambiando el color
-                    //Mostrar el orden del resultado según si es local o visitante
-                    Text(
-                      (partidoBox.isLocal)
-                          ? "${partidoBox.golesAFavor.length}-${partidoBox.golesEnContra.length}"
-                          : "${partidoBox.golesEnContra.length}-${partidoBox.golesAFavor.length}",
-                    ),
-                    //Rival
-                    Text(
-                      partidoBox.rival,
-                      textAlign: TextAlign.center,
-                    ),
-                    //Tipo
-                    Text(
-                      partidoBox.tipoPartido,
-                      textAlign: TextAlign.center,
-                    ),
-                    //Fecha
-                    Text(
-                      partidoBox.fecha,
-                      textAlign: TextAlign.center,
-                    ),
-                    //Hora
-                    Text(
-                      partidoBox.hora,
-                      textAlign: TextAlign.center,
-                      style: TextStyle(fontSize: 10),
-                    ),
-                  ],
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
                 ),
+                child: (DateTime.now()
+                            .difference(DateTime(
+                              int.parse(partidoBox.fecha.split("-")[0]),
+                              int.parse(partidoBox.fecha.split("-")[1]),
+                              int.parse(partidoBox.fecha.split("-")[2]),
+                              //Suponiendo que los partidos duran aproximadamente 1 hora y media
+                              int.parse(partidoBox.hora.split(":")[0]) + 1,
+                              int.parse(partidoBox.hora.split(":")[1]) + 30,
+                            ))
+                            .inSeconds >
+                        0)
+                    ? Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: <Widget>[
+                          //Nosotros
+                          Text(
+                            perfil['nombre_equipo'],
+                            textAlign: TextAlign.center,
+                          ),
+                          //Representar el tipo de partido cambiando el color
+                          //Mostrar el orden del resultado según si es local o visitante
+                          Text(
+                            (partidoBox.isLocal)
+                                ? "${partidoBox.golesAFavor.length}-${partidoBox.golesEnContra.length}"
+                                : "${partidoBox.golesEnContra.length}-${partidoBox.golesAFavor.length}",
+                          ),
+                          //Rival
+                          Text(
+                            partidoBox.rival,
+                            textAlign: TextAlign.center,
+                          ),
+                          //Tipo
+                          Text(
+                            partidoBox.tipoPartido,
+                            textAlign: TextAlign.center,
+                          ),
+                          //Fecha
+                          Text(
+                            partidoBox.fecha,
+                            textAlign: TextAlign.center,
+                          ),
+                        ],
+                      )
+                    : Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: <Widget>[
+                          //Nosotros
+                          Text(
+                            perfil['nombre_equipo'],
+                            textAlign: TextAlign.center,
+                          ),
+                          //Rival
+                          Text(
+                            partidoBox.rival,
+                            textAlign: TextAlign.center,
+                          ),
+                          //Tipo
+                          Text(
+                            partidoBox.tipoPartido,
+                            textAlign: TextAlign.center,
+                          ),
+                          //Fecha
+                          Text(
+                            partidoBox.fecha,
+                            textAlign: TextAlign.center,
+                          ),
+                          //Hora
+                          Text(
+                            partidoBox.hora,
+                            textAlign: TextAlign.center,
+                            style: TextStyle(fontSize: 10),
+                          ),
+                        ],
+                      ),
               ),
             ),
           );

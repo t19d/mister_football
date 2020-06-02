56c555ae47a0aa499f28a8d53bf7d05bb2db529e
David <davidtojo99@gmail.com>
2020-06-02 13:33:37 +0200

    Animación de los detalles.

---
+++ lib/animaciones/animacion_detalles.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class AnimacionDetalles extends PageRouteBuilder {
+  final Widget widget;
+
+  AnimacionDetalles({this.widget})
+      : super(
+          pageBuilder: (
+            BuildContext context,
+            Animation<double> animation,
+            Animation<double> secondaryAnimation,
+          ) =>
+              widget,
+          transitionsBuilder: (
+            BuildContext context,
+            Animation<double> animation,
+            Animation<double> secondaryAnimation,
+            Widget child,
+          ) =>
+              FadeTransition(
+            opacity: animation,
+            child: child,
+          ),
+        );
+}

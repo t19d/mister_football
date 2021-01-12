41710becbc132711361b7ac0eb76db9727fd0a63
David <davidtojo99@gmail.com>
2021-01-12 09:38:36 +0100

    Actualizado diseño

--- lib/routes/ejercicios/w_lista_ejercicios_json.dart
+++ lib/routes/ejercicios/w_lista_ejercicios_json.dart
@@ -11,6 +11,9 @@ class ListaEjerciciosJSON extends StatefulWidget {
 }
 
 class _ListaEjerciciosJSON extends State<ListaEjerciciosJSON> {
+  String valorOrdenarPor = "Duración";
+  List<String> elementosOrdenarPor = ["Duración", "Tipo", "Dificultad"];
+
   Future<String> cargarEjercicios() async {
     return await rootBundle.loadString('assets/json/ejercicios.json');
   }
@@ -30,7 +33,7 @@ class _ListaEjerciciosJSON extends State<ListaEjerciciosJSON> {
     }
     return coloreado.withOpacity(.7);
   }*/
-
+/*
   Widget itemEjercicio(String ejerciciosString) {
     List<dynamic> ejercicios = jsonDecode(ejerciciosString);
     if (ejercicios.length > 0) {
@@ -40,7 +43,241 @@ class _ListaEjerciciosJSON extends State<ListaEjerciciosJSON> {
         shrinkWrap: true,
         children: List.generate(
           ejercicios.length,
+              (iEjercicio) {
+            return InkWell(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => DetallesEjercicioJSON(
+                      datos: ejercicios[iEjercicio],
+                    ),
+                  ),
+                );
+              },
+              child: Container(
+                decoration: BoxDecoration(
+                  border: Border(
+                    top: BorderSide(
+                      color: MisterFootball.colorPrimario,
+                      width: (iEjercicio == 0) ? .4 : 0,
+                    ),
+                    bottom: BorderSide(
+                      color: MisterFootball.colorPrimario,
+                      width: .4,
+                    ),
+                    left: BorderSide(
+                      color: MisterFootball.colorPrimario,
+                      width: .4,
+                    ),
+                    right: BorderSide(
+                      color: MisterFootball.colorPrimario,
+                      width: .4,
+                    ),
+                  ),
+                ),
+                /*margin: EdgeInsets.only(
+                  top: (iEjercicio != 0) ? 2.5 : 0,
+                  bottom: (iEjercicio != (ejercicios.length - 1)) ? 2.5 : 0,
+                ),*/
+                child: Table(
+                  border: TableBorder(
+                    verticalInside: BorderSide(
+                      color: MisterFootball.colorPrimario,
+                      width: .4,
+                    ),
+                  ),
+                  defaultVerticalAlignment: TableCellVerticalAlignment.middle,
+                  children: [
+                    TableRow(
+                      decoration: BoxDecoration(),
+                      children: [
+                        //Título
+                        Container(
+                          padding: EdgeInsets.only(top: 10, bottom: 10),
+                          child: Text(
+                            ejercicios[iEjercicio]['titulo'],
+                            textAlign: TextAlign.center,
+                            /*style: TextStyle(
+                              color: (iEjercicio.isEven) ? Colors.white : MisterFootball.colorPrimario,
+                            ),*/
+                          ),
+                        ),
+                        //Tipo
+                        Text(
+                          ejercicios[iEjercicio]['tipo'],
+                          textAlign: TextAlign.center,
+                          /*style: TextStyle(
+                            color: (iEjercicio.isEven) ? Colors.white : MisterFootball.colorPrimario,
+                          ),*/
+                        ),
+                        //Duración
+                        Text(
+                          "${ejercicios[iEjercicio]['duracion']} min",
+                          textAlign: TextAlign.center,
+                          /*style: TextStyle(
+                            color: (iEjercicio.isEven) ? Colors.white : MisterFootball.colorPrimario,
+                          ),*/
+                        ),
+                        //Dificultad
+                        Text(
+                          ejercicios[iEjercicio]['dificultad'],
+                          textAlign: TextAlign.center,
+                          /*style: TextStyle(
+                            color: (iEjercicio.isEven) ? Colors.white : MisterFootball.colorPrimario,
+                          ),*/
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            );
+            /*return Card(
+            /*shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(15.0),
+            ),*/
+            child: new InkWell(
+              splashColor: MisterFootball.complementario,
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => DetallesEjercicioJSON(datos: ejercicios[iEjercicio],)),
+                );
+              },
+              child: Container(
+                padding: EdgeInsets.all(16.0),
+                decoration: BoxDecoration(
+                  color: colorear(ejercicios[iEjercicio]['tipo']),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: <Widget>[
+                    //Título
+                    Text(
+                      ejercicios[iEjercicio]['titulo'],
+                      textAlign: TextAlign.center,
+                    ),
+                    //Tipo
+                    Text(
+                      ejercicios[iEjercicio]['tipo'],
+                      textAlign: TextAlign.center,
+                      style: TextStyle(fontSize: 10),
+                    ),
+                    //Tiempo
+                    Text(
+                      "${ejercicios[iEjercicio]['duracion']} min",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(fontSize: 10),
+                    ),
+                    //Dificultad
+                    Text(
+                      ejercicios[iEjercicio]['dificultad'],
+                      textAlign: TextAlign.center,
+                      style: TextStyle(fontSize: 10),
+                    ),
+                  ],
+                ),
+              ),
+              //),
+            ),
+          );*/
+          },
+        ),
+      );
+    }
+  }
+*/
+
+  LinearGradient colorearDificultad(String dificultad) {
+    LinearGradient coloreado = LinearGradient(
+      begin: Alignment.topRight,
+      end: Alignment.bottomLeft,
+      colors: [const Color(0xFF000000), const Color(0xFF000000)],
+    );
+    switch (dificultad.toLowerCase()) {
+      case "fácil":
+        coloreado = LinearGradient(
+          begin: Alignment.topRight,
+          end: Alignment.bottomLeft,
+          colors: [const Color(0xFF64ed61), const Color(0xFF78de76)],
+        );
+        break;
+      case "media":
+        coloreado = LinearGradient(
+          begin: Alignment.topRight,
+          end: Alignment.bottomLeft,
+          colors: [const Color(0xFFe6a353), const Color(0xFFdeab6f)],
+        );
+        break;
+      case "difícil":
+        coloreado = LinearGradient(
+          begin: Alignment.topRight,
+          end: Alignment.bottomLeft,
+          colors: [const Color(0xFFeb6c60), const Color(0xFFe38178)],
+        );
+        break;
+    }
+    return coloreado;
+  }
+  Widget itemEjercicio(String ejerciciosString) {
+    List<dynamic> ejercicios = jsonDecode(ejerciciosString);
+    if (ejercicios.length > 0) {
+      return ListView(
+        //No Scroll
+        //physics: NeverScrollableScrollPhysics(),
+        shrinkWrap: true,
+        children: List.generate(
+          ejercicios.length,
           (iEjercicio) {
+            return Padding(
+              padding: EdgeInsets.only(
+                left: MediaQuery.of(context).size.width * .03,
+                right: MediaQuery.of(context).size.width * .03,
+                bottom: 2,
+                top: 2,
+              ),
+              child: Card(
+                elevation: 2.5,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(0.0)),
+                ),
+                child: InkWell(
+                  splashColor: Colors.black12,
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => DetallesEjercicioJSON(
+                          datos: ejercicios[iEjercicio],
+                        ),
+                      ),
+                    );
+                  },
+                  child: Container(
+                    padding: EdgeInsets.all(
+                      MediaQuery.of(context).size.width * .025,
+                    ),
+                    decoration: BoxDecoration(
+                      gradient: colorearDificultad(ejercicios[iEjercicio]['dificultad']),
+                    ),
+                    child: Column(
+                      children: <Widget>[
+                        Text(ejercicios[iEjercicio]['titulo']),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            Text(ejercicios[iEjercicio]['tipo']),
+                            Text("${ejercicios[iEjercicio]['duracion']} min"),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            );
             return InkWell(
               onTap: () {
                 Navigator.push(
@@ -203,7 +440,79 @@ class _ListaEjerciciosJSON extends State<ListaEjerciciosJSON> {
                       print(snapshot.error.toString());
                       return Text(snapshot.error.toString());
                     } else {
-                      return Container(
+                      return Column(
+                        children: <Widget>[
+                          Container(
+                            decoration: BoxDecoration(
+                              border: Border.all(color: MisterFootball.colorSemiprimarioLight2),
+                              borderRadius: BorderRadius.only(
+                                bottomLeft: Radius.circular(10),
+                                bottomRight: Radius.circular(10),
+                              ),
+                            ),
+                            padding: EdgeInsets.only(
+                              left: MediaQuery.of(context).size.width * .04,
+                              right: MediaQuery.of(context).size.width * .04,
+                              top: 10,
+                              bottom: 5,
+                            ),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: <Widget>[
+                                RaisedButton(
+                                  onPressed: () {},
+                                  child: Row(
+                                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                                    children: <Widget>[
+                                      Text("Filtrar"),
+                                      Icon(Icons.filter_list),
+                                    ],
+                                  ),
+                                ),
+                                Row(
+                                  children: <Widget>[
+                                    DropdownButton<String>(
+                                      value: valorOrdenarPor,
+                                      icon: Icon(
+                                        Icons.import_export,
+                                      ),
+                                      underline: Container(
+                                        height: 2,
+                                        color: MisterFootball.colorPrimarioLight2,
+                                      ),
+                                      onChanged: (String v) {
+                                        setState(() {
+                                          valorOrdenarPor = v;
+                                        });
+                                      },
+                                      items: elementosOrdenarPor.map<DropdownMenuItem<String>>((String value) {
+                                        return DropdownMenuItem<String>(
+                                          value: value,
+                                          child: Container(
+                                            padding: EdgeInsets.only(
+                                              right: MediaQuery.of(context).size.width * .03,
+                                              left: MediaQuery.of(context).size.width * .02,
+                                            ),
+                                            alignment: Alignment.center,
+                                            child: Text(
+                                              value,
+                                              textAlign: TextAlign.center,
+                                            ),
+                                          ),
+                                        );
+                                      }).toList(),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ),
+                          Expanded(
+                            child: itemEjercicio(snapshot.data),
+                          )
+                        ],
+                      );
+                      /*return Container(
                         padding: EdgeInsets.all(8),
                         child: SingleChildScrollView(
                           child: Column(
@@ -260,7 +569,7 @@ class _ListaEjerciciosJSON extends State<ListaEjerciciosJSON> {
                             ],
                           ),
                         ),
-                      );
+                      );*/
                     }
                   } else {
                     return Center(
